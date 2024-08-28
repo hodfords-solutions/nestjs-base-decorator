73184705c008600b985634053d53a13b5fcbddb9
phuong-hodfords <phuong.vt@hodfords.com>
2024-08-28 16:50:44 +0700

    feat: upgrade dependencies

--- libs/decorators/value.decorator.ts
+++ libs/decorators/value.decorator.ts
@@ -1,7 +1,7 @@
 import { Payload } from '@nestjs/microservices';
 
 export function Value(): ParameterDecorator {
-    return (target: any, key: string | symbol, descriptor: any): void => {
+    return (target: any, key: string | symbol | undefined, descriptor: any): void => {
         return Payload('value')(target, key, descriptor);
     };
 }
